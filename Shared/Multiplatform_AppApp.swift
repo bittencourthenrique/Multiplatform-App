5888dd2d841496d8dd6cc743d59fe528e732848d
Henrrique Bittencourt <bittencurt.henrique@gmail.com>
2021-10-24 22:28:28 -0300

    Initial Commit

---
+++ Shared/Multiplatform_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiplatform_AppApp.swift
+//  Shared
+//
+//  Created by Henrique Bittencourt on 10/24/21.
+//
+
+import SwiftUI
+
+@main
+struct Multiplatform_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
