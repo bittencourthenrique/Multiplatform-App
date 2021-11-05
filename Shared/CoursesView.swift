f777b9e9d34ab97f139ef60940ecb38ec1279de1
Henrrique Bittencourt <bittencurt.henrique@gmail.com>
2021-11-04 21:22:46 -0300

    Matched Container and zIndex 1

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -22,58 +22,60 @@ struct CoursesView: View {
                     spacing: 16
                 ) {
                     ForEach(courses) { item in
-                        CourseItem(course: item)
-                            .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
-                        .frame(height: 200)
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                show.toggle()
-                                selectedItem = item
-                                isDisabled = true
+                        VStack {
+                            CourseItem(course: item)
+                                .matchedGeometryEffect(id: item.id, in: namespace, isSource: !show)
+                            .frame(height: 200)
+                            .onTapGesture {
+                                withAnimation(.spring(response: 0.3, dampingFraction: 0.7, blendDuration: 0)) {
+                                    show.toggle()
+                                    selectedItem = item
+                                    isDisabled = true
+                                }
                             }
-                        }
                         .disabled(isDisabled)
+                        }
+                        .matchedGeometryEffect(id: "container\(item.id)", in: namespace, isSource: !show)
                     }
                 }
                 .padding(16)
                 .frame(maxWidth: .infinity)
             }
+            zIndex(1)
             if selectedItem != nil {
-                ScrollView {
-                    CourseItem(course: selectedItem!)
-                        .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
-                        .frame(height: 300)
-                        .onTapGesture {
-                            withAnimation(.spring()) {
-                                show.toggle()
-                                selectedItem = nil
-                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                                    isDisabled = false
+                ZStack {
+                    VStack {
+                        ScrollView {
+                            CourseItem(course: selectedItem!)
+                                .matchedGeometryEffect(id: selectedItem!.id, in: namespace)
+                                .frame(height: 300)
+                            VStack {
+                                ForEach(0 ..< 20) { item in
+                                    CourseRow()
                                 }
                             }
-                        }
-                    VStack {
-                        ForEach(0 ..< 20) { item in
-                            CourseRow()
+                            .padding()
                         }
                     }
-                    .padding()
-                }
-                .background(Color("Background 1"))
-                .transition(
-                    .asymmetric(
-                        insertion: AnyTransition
-                                    .opacity
-                                    .animation(.spring().delay(0.3)),
-                        removal: AnyTransition
-                            .opacity
-                            .animation(.spring())
-                    )
-                )
+                    .background(Color("Background 1"))
+                    .clipShape(RoundedRectangle(cornerRadius: 22, style: .continuous))
+                    .matchedGeometryEffect(id: "container\(selectedItem!.id)", in: namespace)
                 .edgesIgnoringSafeArea(.all)
+                zIndex(2)
+                    
+                CloseButton()
+                    .onTapGesture {
+                        withAnimation(.spring()) {
+                            show.toggle()
+                            selectedItem = nil
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                isDisabled = false
+                            }
+                        }
+                    }
+                }
             }
         }
-        
     }
 }
 
